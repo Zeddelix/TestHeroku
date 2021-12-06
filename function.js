3ff31f4dc9d2b52136fe33ac490d49d266dcf888
Gaulthier Petitdemange <gaulthier.petitdemange@uha.fr>
2021-12-06 12:12:43 +0100

    qzd

--- function.js
+++ function.js
@@ -6,3 +6,37 @@ const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
 const readBtn = document.getElementById('readBtn');
 const removeBtn = document.getElementById('removeBtn');
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
+
+
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+const firebaseConfig = {
+  apiKey: "AIzaSyAA6ivv8uB1lBTxoaT2MyjqkSj4YJD3e5Y",
+
+    authDomain: "projetcloudcompu.firebaseapp.com",
+
+    projectId: "projetcloudcompu",
+
+    storageBucket: "projetcloudcompu.appspot.com",
+
+    messagingSenderId: "583578801932",
+
+    appId: "1:583578801932:web:0bf2ed50e8c97aa16ddb29",
+
+    measurementId: "G-RCVK6QZE80"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+function writeUserData(){
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: "1",
+    email: "2"
+  });
+}
